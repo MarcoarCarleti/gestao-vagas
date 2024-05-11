d98f4275121ba964503e9375532a18c3aece6c7b
MarcoarCarleti <marcarleti@gmail.com>
2024-05-11 12:32:56 -0300

    first commit

---
+++ src/test/java/br/com/marcocarleti/gestao_vagas/GestaoVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marcocarleti.gestao_vagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoVagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
